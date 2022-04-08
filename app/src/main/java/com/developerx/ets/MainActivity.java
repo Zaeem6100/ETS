d5157005b11512735044e0ac94ff64c093342a5c
Zaeem <32678310+Zaeem6100@users.noreply.github.com>
2022-04-08 16:04:49 +0500

    Python Setup

--- app/src/main/java/com/developerx/ets/MainActivity.java
+++ app/src/main/java/com/developerx/ets/MainActivity.java
@@ -33,22 +33,20 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        switch (requestCode) {
-            case RequestPermissionCode:
-                if (grantResults.length > 0) {
-                    boolean CameraPermission = grantResults[0] == PackageManager.PERMISSION_GRANTED;
-                    boolean InternetPermission = grantResults[1] == PackageManager.PERMISSION_GRANTED;
-                    boolean ExternalStoragePermissionRead = grantResults[2] == PackageManager.PERMISSION_GRANTED;
-                    boolean ExternalStoragePermissionWrite = grantResults[3] == PackageManager.PERMISSION_GRANTED;
+        if (requestCode == RequestPermissionCode) {
+            if (grantResults.length > 0) {
+                boolean CameraPermission = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+                boolean InternetPermission = grantResults[1] == PackageManager.PERMISSION_GRANTED;
+                boolean ExternalStoragePermissionRead = grantResults[2] == PackageManager.PERMISSION_GRANTED;
+                boolean ExternalStoragePermissionWrite = grantResults[3] == PackageManager.PERMISSION_GRANTED;
 
-                    if (CameraPermission && InternetPermission && ExternalStoragePermissionRead && ExternalStoragePermissionWrite) {
-                        Toast.makeText(MainActivity.this, "Permission Granted", Toast.LENGTH_LONG).show();
-                    } else {
-                        Toast.makeText(MainActivity.this, "Permission Denied", Toast.LENGTH_LONG).show();
-                        finish();
-                    }
+                if (CameraPermission && InternetPermission && ExternalStoragePermissionRead && ExternalStoragePermissionWrite) {
+                    Toast.makeText(MainActivity.this, "Permission Granted", Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(MainActivity.this, "Permission Denied", Toast.LENGTH_LONG).show();
+                    finish();
                 }
-                break;
+            }
         }
     }
 
@@ -64,7 +62,7 @@ public class MainActivity extends AppCompatActivity {
         return FirstPermissionResult == PackageManager.PERMISSION_GRANTED &&
                 SecondPermissionResult == PackageManager.PERMISSION_GRANTED &&
                 ThirdPermissionResult == PackageManager.PERMISSION_GRANTED &&
-                FirstPermissionResult == PackageManager.PERMISSION_GRANTED;
+                FourthPermissionResult == PackageManager.PERMISSION_GRANTED;
     }
 
 
